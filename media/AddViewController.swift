98f2540b25567fc41109b94b7525c2acd5c74791
Etienne Jézéquel <etiennejezequel@MacBook-Pro-de-Etienne.local>
2017-03-08 14:22:03 +0100

    Mise à jour de l’ajout de film

--- media/AddViewController.swift
+++ media/AddViewController.swift
@@ -11,6 +11,7 @@ import Firebase
 
 class AddViewController: UIViewController {
     var ref: FIRDatabaseReference!
+    let baseURL = "https://www.omdbapi.com"
 
     @IBOutlet weak var input: UITextField!
     var count:Int = 0
@@ -23,14 +24,25 @@ class AddViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func getJSON(urlToRequest:String) -> Data {
+        let data = try? Data(contentsOf: URL(string: urlToRequest)!)
+        return data!
+    }
+    
+    func parseJSON(inputData:Data) -> Dictionary<String,String> {
+        let dictData = (try! JSONSerialization.jsonObject(with: inputData, options: .mutableContainers)) as! Dictionary<String,String>
+        return dictData
     }
     
     @IBAction func add(_ sender: Any) {
         if !(self.input.text?.isEmpty)!{
-            let film = self.input.text?.replacingOccurrences(of: " ", with: "+")
+            let film:String = (self.input.text?.replacingOccurrences(of: " ", with: "+"))!
+            let data = parseJSON(inputData: getJSON(urlToRequest: baseURL + "/?t="+film+"&y=&plot=short&r=json"))
+
             let key = ref.child("medias").childByAutoId().key
-            let childUpdates = ["medias/\(key)/": ["name":film]]
+            let childUpdates = ["medias/\(key)/": ["title":data["Title"],"id":key, "runtime":data["Runtime"], "genre":data["Genre"],"country":data["Country"],"released":data["Released"],"plot":data["Plot"],"poster":data["Poster"],"rating":data["imdbRating"],"year":data["Year"]]]
             ref.updateChildValues(childUpdates)
             self.input.text=""
         }
---
+++ media/Movie.swift
@@ -0,0 +1,36 @@
+//
+//  Movie.swift
+//  media
+//
+//  Created by Etienne Jézéquel on 08/03/2017.
+//  Copyright © 2017 Etienne Jézéquel. All rights reserved.
+//
+
+import Foundation
+
+class Movie {
+    var title:String
+    var year:String
+    var poster:String
+    var rating:String
+    var plot:String
+    var runtime:String
+    var released:String
+    var genre:String
+    var country:String
+    
+    init(title:String, year:String, poster:String, rating:String, plot:String, runtime:String, released:String, genre:String, country:String) {
+        self.country = country
+        self.year = year
+        self.title = title
+        self.poster = poster
+        self.rating = rating
+        self.plot = plot
+        self.runtime = runtime
+        self.released = released
+        self.genre = genre
+    }
+    
+
+    
+}
