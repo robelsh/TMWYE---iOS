31a33cd579b6e52defd107f2ef1bbb5b690ee43d
Etienne Jézéquel <etiennejezequel@macbook-pro-de-etienne.home>
2017-03-08 22:45:31 +0100

    Vérification d’internet

--- media/LoginViewController.swift
+++ media/LoginViewController.swift
@@ -19,31 +19,47 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate {
     var ref: FIRDatabaseReference!
 
     @IBAction func logIn(_ sender: Any) {
-        if self.loginTextField.text == "" || self.passwordTextField.text == "" {
-            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
-            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-            alertController.addAction(defaultAction)
-            self.present(alertController, animated: true, completion: nil)
-            
+        if Reachability.isConnectedToNetwork() != true {
+            self.displayAlertNetwork()
         } else {
-            FIRAuth.auth()?.signIn(withEmail: self.loginTextField.text!, password: self.passwordTextField.text!) { (user, error) in
+            if self.loginTextField.text == "" || self.passwordTextField.text == "" {
+                let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+            
+            } else {
+                FIRAuth.auth()?.signIn(withEmail: self.loginTextField.text!, password: self.passwordTextField.text!) { (user, error) in
                 
-                if error == nil {
-                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
-                    self.present(vc!, animated: true, completion: nil)
+                    if error == nil {
+                        let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                        self.present(vc!, animated: true, completion: nil)
                     
-                } else {
-                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                    alertController.addAction(defaultAction)
-                    self.present(alertController, animated: true, completion: nil)
+                    } else {
+                        let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        alertController.addAction(defaultAction)
+                        self.present(alertController, animated: true, completion: nil)
+                    }
                 }
             }
         }
     }
     
+    func displayAlertNetwork(){
+        let alert = UIAlertController(title: "No Internet Connection", message: "Make sure your device is connected to the internet.", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(defaultAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if Reachability.isConnectedToNetwork() != true {
+            self.displayAlertNetwork()
+        }
+        
         GIDSignIn.sharedInstance().uiDelegate = self
         
         FIRAuth.auth()?.addStateDidChangeListener() { (auth, user) in
@@ -55,7 +71,12 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate {
     }
     
     @IBAction func logInGoogle(_ sender: Any) {
-        GIDSignIn.sharedInstance().signIn()
+        if Reachability.isConnectedToNetwork() != true {
+            self.displayAlertNetwork()
+
+        }else {
+            GIDSignIn.sharedInstance().signIn()
+        }
     }
 
     override func didReceiveMemoryWarning() {
--- media/SignUpViewController.swift
+++ media/SignUpViewController.swift
@@ -22,33 +22,45 @@ class SignUpViewController: UIViewController {
     }
     
     @IBAction func signUp(_ sender: Any) {
-        if emailTextField.text == "" {
-            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
-            
-            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-            alertController.addAction(defaultAction)
-            
-            present(alertController, animated: true, completion: nil)
-            
-        } else {
-            FIRAuth.auth()?.createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+        if Reachability.isConnectedToNetwork() != true {
+            self.displayAlertNetwork()
+        }else {
+            if emailTextField.text == "" {
+                let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
                 
-                if error == nil {
-                    self.ref = FIRDatabase.database().reference()
-                    let user:User = User(uid: (FIRAuth.auth()?.currentUser?.uid)!, displayname: "", email: (FIRAuth.auth()?.currentUser?.email)!, photoUrl: "", providerID: (FIRAuth.auth()?.currentUser?.providerID)!)
-                    self.ref.child("users").child(user.uid).setValue(["displayName": user.displayName, "uid": user.uid, "email":user.email, "providerId":user.providerID, "photoURL": user.photoUrl])
-                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
-                    self.present(vc!, animated: true, completion: nil)
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                present(alertController, animated: true, completion: nil)
+                
+            } else {
+                FIRAuth.auth()?.createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
                     
-                } else {
-                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-                    
-                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                    alertController.addAction(defaultAction)
-                    self.present(alertController, animated: true, completion: nil)
+                    if error == nil {
+                        self.ref = FIRDatabase.database().reference()
+                        let user:User = User(uid: (FIRAuth.auth()?.currentUser?.uid)!, displayname: "", email: (FIRAuth.auth()?.currentUser?.email)!, photoUrl: "", providerID: (FIRAuth.auth()?.currentUser?.providerID)!)
+                        self.ref.child("users").child(user.uid).setValue(["displayName": user.displayName, "uid": user.uid, "email":user.email, "providerId":user.providerID, "photoURL": user.photoUrl])
+                        let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                        self.present(vc!, animated: true, completion: nil)
+                        
+                    } else {
+                        let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                        
+                        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        alertController.addAction(defaultAction)
+                        self.present(alertController, animated: true, completion: nil)
+                    }
                 }
             }
+
         }
+    }
+    
+    func displayAlertNetwork(){
+        let alert = UIAlertController(title: "No Internet Connection", message: "Make sure your device is connected to the internet.", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(defaultAction)
+        self.present(alert, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
