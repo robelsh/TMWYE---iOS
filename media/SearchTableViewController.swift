488fce741337c4cd030bb4ad9155397a0afd50ef
Etienne Jézéquel <etienne.jezequel@icloud.com>
2017-03-14 20:33:49 +0100

    #9 Mise à jour de la recherche

--- media/SearchTableViewController.swift
+++ media/SearchTableViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class SearchTableViewController: UITableViewController {
 
     var search:Bool = false
-    let baseURL = "https://api.themoviedb.org/3/search/movie?api_key=72e58ed9123ba68d1f814768448360c0&query="
+    let baseURL = "https://api.themoviedb.org/3/"
     var movies:[Movie] = []
     let searchController = UISearchController(searchResultsController: nil)
     
@@ -73,7 +73,33 @@ class SearchTableViewController: UITableViewController {
         if cell != nil {
             let indexPath = tableView.indexPath(for: cell!)
             let svc = segue.destination as! DetailViewController;
-            svc.movie = self.movies[(indexPath?.row)!]
+            let film:String = self.movies[(indexPath?.row)!].imdbId
+            let dictData = (try! JSONSerialization.jsonObject(with: getJSON(urlToRequest: baseURL+"movie/"+film+"?api_key=72e58ed9123ba68d1f814768448360c0"), options: .mutableContainers)) as? [String: Any]
+            let movie:Movie = Movie()
+            if let datas = dictData {
+                movie.title = datas["title"] as! String
+                if let runtime = datas["runtime"] as! NSNumber? {
+                    movie.runtime = runtime.stringValue
+                }
+                if let poster = datas["poster_path"] as! String? {
+                    movie.poster = try! Data(contentsOf:  URL(string: "https://image.tmdb.org/t/p/w500"+poster)!)
+                }
+                movie.plot = datas["overview"] as! String
+                movie.released = datas["release_date"] as! String
+                if let rating = datas["vote_average"] as! NSNumber? {
+                    movie.rating = rating.stringValue
+                }
+                
+                if let genre = datas["genres"] as? [[String:Any]] {
+                    for i in 0...genre.count-1 {
+                        let genreItem = genre[i]["name"] as! String
+                        movie.genre = movie.genre + " " + genreItem
+                    }
+                }
+                
+                svc.movie = movie
+
+            }
         }
     }
 }
@@ -90,12 +116,14 @@ extension SearchTableViewController: UISearchResultsUpdating {
         if let searchBarText = self.searchController.searchBar.text {
             if(searchBarText != ""){
                 let film:String = (searchBarText.replacingOccurrences(of: " ", with: "+"))
-                let dictData = (try! JSONSerialization.jsonObject(with: getJSON(urlToRequest: baseURL + film), options: .mutableContainers)) as? [String: Any]
+                let dictData = (try! JSONSerialization.jsonObject(with: getJSON(urlToRequest: baseURL+"search/movie?api_key=72e58ed9123ba68d1f814768448360c0&query="+film), options: .mutableContainers)) as? [String: Any]
                 let results = dictData?["results"] as! [Dictionary<String,Any>]
 
                 for i in 0...results.count-1 {
                     let movie = Movie()
                     movie.title = results[i]["title"] as! String
+                    let id = results[i]["id"] as! NSNumber
+                    movie.imdbId = id.stringValue
                     if let year = results[i]["release_date"] as! String? {
                         if(year != ""){
                             let startIndex = year.index(year.startIndex, offsetBy: 4)
